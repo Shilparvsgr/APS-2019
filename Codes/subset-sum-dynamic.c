de1b4104d0887bfe4542c3189cf92b7cbe30727e
Shilparvsgr <35289582+Shilparvsgr@users.noreply.github.com>
2019-02-09 19:52:38 +0530

    Create subset-sum-dynamic.c

---
+++ Codes/subset-sum-dynamic.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,sum;
+    printf("Enter the number of integers to be used to obtain the sum");
+    scanf("%d",&n);
+    int a[n+1];
+    int i,j;
+    printf("Enter the numbers to find subset sum");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the sum to be obtained");
+    scanf("%d",&sum);
+    int result[sum+1];
+    for(i=0;i<=sum;i++){
+        result[i]=0;
+    }
+    result[0] = 1;
+    for(j=0;j<n;j++){
+    for(i = a[j]; i <= sum; i++)
+
+        result[i] = result[i] + result[i-a[j]];
+}
+
+
+    printf("Number of subsets is %d ",result[sum]);
+}
